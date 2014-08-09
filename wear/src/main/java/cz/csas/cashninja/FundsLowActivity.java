36e13018b417deaddd4ce0415122e7158d3fe690
lubenda <lbenda@csas.cz>
2014-08-09 19:31:49 +0200

    funds low activity
    onClick

--- wear/src/main/java/cz/csas/cashninja/FundsLowActivity.java
+++ wear/src/main/java/cz/csas/cashninja/FundsLowActivity.java
@@ -1,9 +1,14 @@
 package cz.csas.cashninja;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.wearable.view.CircledImageView;
+import android.support.wearable.view.WatchViewStub;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 import cz.csas.cashninja.R;
 
 public class FundsLowActivity extends Activity {
@@ -12,6 +17,28 @@ public class FundsLowActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.insufficient_funds);
+
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+
+
+                CircledImageView mCircledImageView = (CircledImageView) stub.findViewById(R.id.insf_circle);
+
+                //Listener to send the message (it is just an example)
+                mCircledImageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        Intent intent = new Intent(FundsLowActivity.this, AccountTypeActivity.class);
+                        startActivity(intent);
+
+                    }
+                });
+            }
+        });
     }
 
 
