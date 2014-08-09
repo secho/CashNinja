e2ab6a19cda4ac5ab7cc7ca78c1934c3931cfa71
unknown <cen35826@CENN2581.cen.csin.cz>
2014-08-09 17:52:59 +0200

    New activities

---
+++ wear/src/main/java/cz/csas/cashninja/ConfirmActivity.java
@@ -0,0 +1,24 @@
+package cz.csas.cashninja;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class ConfirmActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub_confirm);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
---
+++ wear/src/main/java/cz/csas/cashninja/TransferActivity.java
@@ -0,0 +1,46 @@
+package cz.csas.cashninja;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.wearable.view.DelayedConfirmationView;
+import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.TextView;
+
+public class TransferActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub_transfer);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+
+        final DelayedConfirmationView dcv = (DelayedConfirmationView) stub.findViewById(R.id.delayed_confirmation);
+        dcv.setTotalTimeMs(3000);
+        dcv.setListener(new DelayedConfirmationView.DelayedConfirmationListener() {
+            @Override
+            public void onTimerFinished(View view) {
+                Intent intent = new Intent(TransferActivity.this, ConfirmActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onTimerSelected(View view) {
+
+            }
+        });
+
+
+    }
+
+
+}
